4260630e5a7aea272576f1088d8f9558e98dc66c
John Buevich <johnbuevich@yahoo.com>
2018-11-02 00:53:56 +0300

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: __dirname + '/aaa',
+        path: __dirname,
         filename: './main2.js'
     },
 
