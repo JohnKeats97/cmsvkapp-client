60f5d79bf86748af8332a7b98015ee0b2b5a91df
John Buevich <johnbuevich@yahoo.com>
2018-11-02 00:58:15 +0300

    midle server

--- src/components/WindowApp/LoadingWindow/index.js
+++ src/components/WindowApp/LoadingWindow/index.js
@@ -17,7 +17,7 @@ export default class LoadingWindow extends React.Component {
             return;
         }
         let myMenu = {};
-        Fetch.Get('http://localhost:5000/service/10000/menu/?data=menu', {
+        Fetch.Get('http://ec2-13-58-118-146.us-east-2.compute.amazonaws.com/service/10000/menu/?data=menu', {
             'x-client-authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImQ4YjQwMWIyNjcyOTUxYWZmMDVhYThmYjM0OTE3Yjg2ZWUzZGZmYTEyMGU2ZmVjMTA3OTY5NzNmZWNmYzFlNTVlMmE2MmZiNmQ2MWJjOTAwIn0.eyJhdWQiOiJlZHVjYXRpb25fYnVldmljaF9ndXNldl9wYXJmZW5rb3YiLCJqdGkiOiJkOGI0MDFiMjY3Mjk1MWFmZjA1YWE4ZmIzNDkxN2I4NmVlM2RmZmExMjBlNmZlYzEwNzk2OTczZmVjZmMxZTU1ZTJhNjJmYjZkNjFiYzkwMCIsImlhdCI6MTU0MDM3MTU5NSwibmJmIjoxNTQwMzcxNTk1LCJleHAiOjE1NDA1NDQzOTUsInN1YiI6IiIsInNjb3BlcyI6WyJmdWxsX2FjY2VzcyJdfQ.cmCCs3z5QKw1hBhBE48Rw1IBbqaHALLr8To60C0NzkYSVAmmT-FtC8I-6KE01tV_nWlxciGVUC10uIYM5TzfZslAjRQxJeY5ztU96tvJUCqWuOZHA4sf_BJ5BsatKNVaknO1kJ92gmLPb_BglP_LowC-rNFmNck2S4SDCqQExWQ'
         })
             .then(({response}) => {
@@ -29,7 +29,7 @@ export default class LoadingWindow extends React.Component {
                     }
                     myMenu[menu[i].title] = cat;
                 }
-                Fetch.Get('http://localhost:5000/service/10000/menu/?data=products', {
+                Fetch.Get('http://ec2-13-58-118-146.us-east-2.compute.amazonaws.com/service/10000/menu/?data=products', {
                         'x-client-authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImQ4YjQwMWIyNjcyOTUxYWZmMDVhYThmYjM0OTE3Yjg2ZWUzZGZmYTEyMGU2ZmVjMTA3OTY5NzNmZWNmYzFlNTVlMmE2MmZiNmQ2MWJjOTAwIn0.eyJhdWQiOiJlZHVjYXRpb25fYnVldmljaF9ndXNldl9wYXJmZW5rb3YiLCJqdGkiOiJkOGI0MDFiMjY3Mjk1MWFmZjA1YWE4ZmIzNDkxN2I4NmVlM2RmZmExMjBlNmZlYzEwNzk2OTczZmVjZmMxZTU1ZTJhNjJmYjZkNjFiYzkwMCIsImlhdCI6MTU0MDM3MTU5NSwibmJmIjoxNTQwMzcxNTk1LCJleHAiOjE1NDA1NDQzOTUsInN1YiI6IiIsInNjb3BlcyI6WyJmdWxsX2FjY2VzcyJdfQ.cmCCs3z5QKw1hBhBE48Rw1IBbqaHALLr8To60C0NzkYSVAmmT-FtC8I-6KE01tV_nWlxciGVUC10uIYM5TzfZslAjRQxJeY5ztU96tvJUCqWuOZHA4sf_BJ5BsatKNVaknO1kJ92gmLPb_BglP_LowC-rNFmNck2S4SDCqQExWQ'
                     })
                         .then(({response})=>{
